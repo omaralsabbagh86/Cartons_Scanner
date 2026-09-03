c60d662b93daeab6164e3eb0b9420dd8e59a0405
omaralsabbagh86 <omar.alsabbagh86@gmail.com>
2026-09-03 08:37:35 +0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-var CACHE_NAME = "carton-scanner-v51";
-var ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png", "./seed-data.json"];
+var CACHE_NAME = "carton-scanner-v52";
+var ASSETS = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", function(event){
   self.skipWaiting();
